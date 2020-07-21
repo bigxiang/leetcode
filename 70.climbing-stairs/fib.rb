1dfcd8ffc84a53f1cbdf27a8f404e8850a2dcd23
Xiang Li <bigxiang@gmail.com>
2020-07-21 20:49:00 +0800

    Update 70

--- 70.climbing-stairs/fib.rb
+++ 70.climbing-stairs/fib.rb
@@ -8,11 +8,9 @@
 # @param {Integer} n
 # @return {Integer}
 def climb_stairs(n)
-  return n if n <= 2
-
-  n_1 = 1
-  n_2 = 2
-  (3..n).each do |i|
+  n_1 = 0
+  n_2 = 1
+  (1..n).each do |i|
     n_1, n_2 = n_2, n_1 + n_2
   end
   n_2
