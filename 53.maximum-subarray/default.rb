9e35e969ad077a96acd463ddbc5cef4b7a063509
Xiang Li <bigxiang@gmail.com>
2020-07-23 19:06:11 +0800

    Add 53

---
+++ 53.maximum-subarray/default.rb
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=53 lang=ruby
+#
+# [53] Maximum Subarray
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer}
+def max_sub_array(nums)
+  max = -2147483648
+  current_max = -2147483648
+  nums.each do |n|
+    current_max = current_max + n > n ? current_max + n : n
+    max = current_max > max ? current_max : max
+  end
+  max
+end
+# @lc code=end
+
