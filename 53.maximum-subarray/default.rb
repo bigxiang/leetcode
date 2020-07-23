53757de3c39255a8b7915e92bf64643f604d93dc
Xiang Li <bigxiang@gmail.com>
2020-07-23 20:03:37 +0800

    Improve 53

--- 53.maximum-subarray/default.rb
+++ 53.maximum-subarray/default.rb
@@ -9,11 +9,13 @@
 # @return {Integer}
 def max_sub_array(nums)
   max = -2147483648
-  current_max = -2147483648
+  current_max = 0
+
   nums.each do |n|
     current_max = current_max + n > n ? current_max + n : n
     max = current_max > max ? current_max : max
   end
+
   max
 end
 # @lc code=end
