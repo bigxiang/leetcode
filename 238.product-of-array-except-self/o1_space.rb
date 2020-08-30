41aa7e330773d8249ae18ef30e41ee4e4d96e0eb
Xiang Li <bigxiang@gmail.com>
2020-08-30 21:44:33 +0800

    Update 238

--- 238.product-of-array-except-self/o1_space.rb
+++ 238.product-of-array-except-self/o1_space.rb
@@ -8,17 +8,15 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def product_except_self(nums)
-  result = []
-
-  result[0] = 1
-  (1...nums.size).each do |i|
-    result[i] = result[i-1] * nums[i-1]
+  result = (1...nums.size).reduce([1]) do |memo, i|
+    memo[i] = memo[i-1] * nums[i-1]
+    memo
   end
 
-  suffix_products = 1
+  suffix_product = 1
   (nums.size-2).downto(0) do |i|
-    suffix_products *= nums[i+1]
-    result[i] *= suffix_products
+    suffix_product *= nums[i+1]
+    result[i] *= suffix_product
   end
 
   result
