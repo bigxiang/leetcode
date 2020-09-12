306ff364dba8e86f27050be92955084b1638d7b5
Xiang Li <bigxiang@gmail.com>
2020-09-12 16:45:40 +0800

    Update 146

--- 146.lru-cache/default.rb
+++ 146.lru-cache/default.rb
@@ -6,61 +6,22 @@
 
 # @lc code=start
 class LRUCache
-    class DoubleLinkedList
-        attr_accessor :head, :tail, :size
-
-        def initialize
-            @head = ListNode.new
-            @tail = ListNode.new
-            head.next = tail
-            tail.prev = head
-            @size = 0
-        end
-
-        def empty?
-            head.next == tail
-        end
-
-        def add(new_node, node = head)
-            node.next.prev = new_node
-            new_node.next = node.next
-            node.next = new_node
-            new_node.prev = node
-            @size += 1
-        end
-
-        def delete(node)
-            return if empty?
-
-            prev_node = node.prev
-            prev_node.next = node.next
-            node.next.prev = prev_node
-            @size -= 1
-            node
-        end
-
-        def delete_last
-            delete(tail.prev)
-        end
-    end
+    attr_reader :capacity, :cache, :head, :tail
 
     class ListNode
-        attr_accessor :key, :val, :prev, :next
-
-        def initialize(key = nil, val = nil)
-            @key = key
-            @val = val
-        end
+      attr_accessor :prev, :next, :key, :value
     end
 
-    attr_reader :cache, :capacity, :cache_list
 =begin
     :type capacity: Integer
 =end
     def initialize(capacity)
-        @capacity = capacity
-        @cache = {}
-        @cache_list = DoubleLinkedList.new
+      @capacity = capacity
+      @head = ListNode.new
+      @tail = ListNode.new
+      @head.next = @tail
+      @tail.prev = @head
+      @cache = {}
     end
 
 
@@ -69,11 +30,11 @@ class LRUCache
     :rtype: Integer
 =end
     def get(key)
-        return -1 if !cache.has_key?(key)
+      return -1 if !cache.key?(key)
 
-        node = cache[key]
-        move_to_head(node)
-        node.val
+      node = cache[key]
+      move_to_head(node)
+      node.value
     end
 
 
@@ -83,38 +44,61 @@ class LRUCache
     :rtype: Void
 =end
     def put(key, value)
-        if node = cache[key]
-            node.val = value
-            move_to_head(node)
-        else
-            delete_last if full?
-            node = ListNode.new(key, value)
-            cache[key] = node
-            cache_list.add(node)
-        end
+      if cache.key?(key)
+        node = cache[key]
+        node.value = value
+        move_to_head(node)
+      else
+        node = build_new_node(key, value)
+        delete(tail.prev) if full?
+        add_after_head(node)
+      end
     end
 
     private
 
-    def full?
-        cache_list.size == capacity
-    end
-
     def move_to_head(node)
-        cache_list.delete(node)
-        cache_list.add(node)
+      delete(node) && add_after_head(node)
     end
 
-    def delete_last
-        node = cache_list.delete_last
-        cache.delete(node.key)
+    def empty?
+      size == 0
+    end
+
+    def full?
+      size == capacity
+    end
+
+    def size
+      cache.size
+    end
+
+    def delete(node)
+      return false if empty?
+
+      node.prev.next = node.next
+      node.next.prev = node.prev
+      cache.delete(node.key)
+    end
+
+    def add_after_head(node)
+      node.next = head.next
+      head.next.prev = node
+      head.next = node
+      node.prev = head
+      cache[node.key] = node
+    end
+
+    def build_new_node(key, value)
+      node = ListNode.new
+      node.key = key
+      node.value = value
+      node
     end
 end
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache.new(capacity)
 # param_1 = obj.get(key)
 # obj.put(key, value)
 # @lc code=end
-
